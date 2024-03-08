6fc63416a967df333dfe2316c9b8f95b4be6f6f1
dominicbeesley <dominic@authorityfile.co.uk>
2024-03-08 17:27:58 +0000

    ADD SDT target and copy over code from Hoglet's repo

--- src/MMC_Driver.asm
+++ src/MMC_Driver.asm
@@ -9,7 +9,6 @@
 
 ;;; MMC settings
 _VIA_BASE  =VIABASE             ; Base Address of 6522 VIA
-_TURBOMMC  =0                   ; 1 = build for TurboMMC, and enable PB2-4 as outputs
 MAX_DRIVES          = 2         ; Don't make this bigger than 2 or the drive table below will overflow
 
 attempts%           = &C2E9     ; 1 byte
